030240cb47ddf94b5d8e7dfdea2f2470f1d4b243
george-prado <gmeloprado@gmail.com>
2024-03-10 17:32:36 -0300

    feat: main application architecture

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/george-prado/gin-api-rest/routes"
+import (
+	"github.com/george-prado/gin-api-rest/initializers"
+	"github.com/george-prado/gin-api-rest/migrate"
+	"github.com/george-prado/gin-api-rest/routes"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+var (
+	server *gin.Engine
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+	
+	initializers.ConnectDB(&config)
+	
+	
+	server = gin.Default()
+}
 
 func main() {
+	migrate.MigrateDB()
+	
 	routes.HandleRequests()
 }
