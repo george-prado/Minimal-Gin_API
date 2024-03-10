9cd4c43f9ee560780dfaf18f06145f4649f44158
george-prado <gmeloprado@gmail.com>
2024-03-10 17:30:47 -0300

    feat: implement CRUD controllers & route maping

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,17 +1,105 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/george-prado/gin-api-rest/initializers"
+	"github.com/george-prado/gin-api-rest/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func ShowStudents(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"name": "John Cena",
-	})
+func GetAllStudents(c *gin.Context) {
+	var students []models.Student
+	initializers.DB.Find(&students)
+	c.JSON(200, students)
 }
 
-func Greetings(c *gin.Context) {
-	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		name: "Do you always come here?",
-	})
+func PostStudent(c *gin.Context){
+	var student models.Student
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	initializers.DB.Create(&student)
+	c.JSON(http.StatusOK, student)
+}
+
+func GetStudentById(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	initializers.DB.First(&student, id)
+	
+	if student.Id == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Error": "Student not found"})
+		return
+	}
+	
+	c.JSON(http.StatusOK, student)
+}
+
+func GetStudentsByAge(c *gin.Context){
+	var students []models.Student
+	ageStr :=  c.Param("age")
+	
+	if ageStr == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error": "You must provide an age"})
+		return
+	}
+	
+	initializers.DB.Where("age = ?", ageStr).Find(&students)
+	
+	if len(students) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error": "Student not found"})
+		return
+	}
+	
+	c.JSON(http.StatusOK, students)
+}
+
+func UpdateStudentById(c *gin.Context){
+	var student models.Student
+	id := c.Params.ByName("id")
+	initializers.DB.First(&student, id)
+	
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	
+	initializers.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, gin.H{
+		"success": "student info updated"})
+}
+
+func DeleteStudentById(c *gin.Context){
+	var student models.Student
+	id := c.Params.ByName("id")
+	initializers.DB.First(&student, id)
+	
+	if student.Id != 0 {
+		initializers.DB.Delete(&student)
+		c.JSON(http.StatusOK, gin.H{
+			"Success": "Student deleted"})
+		return
+	}
+	
+	c.JSON(http.StatusNotFound, gin.H{
+		"Error": "Student not found"})
+}
+
+func DeleteAll(c *gin.Context) {
+	var students []models.Student
+	
+	initializers.DB.Find(&students)
+	
+	for _, student := range students {
+		initializers.DB.Delete(&student)
+	}
+	
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Cleared database"})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,21 @@ package routes
 import (
 	"github.com/george-prado/gin-api-rest/controllers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func HandleRequests() {
 	r := gin.Default()
-	r.GET("/students", controllers.ShowStudents)
-	r.GET("/:name", controllers.Greetings)
-
+	r.GET("/api/healthchecker", func(ctx *gin.Context) {
+		message := "Welcome to Golang with Gorm and Postgres"
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
+	})
+	r.GET("/students/all", controllers.GetAllStudents)
+	r.GET("/students/id/:id", controllers.GetStudentById)
+	r.GET("/students/age/:age", controllers.GetStudentsByAge)
+	r.POST("/student/create", controllers.PostStudent)
+	r.PUT("/students/update/:id", controllers.UpdateStudentById)
+	r.DELETE("/students/delete/:id", controllers.DeleteStudentById)
+	r.DELETE("/data/clearall", controllers.DeleteAll)
 	r.Run()
 }
